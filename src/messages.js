e47b461554537d278f2b9a8c885cabd508350027
ClintTCG <clint@tcgplayer.com>
2018-06-22 17:27:47 -0600

    Initial commit of first judge-bingo prototype

---
+++ src/messages.js
@@ -0,0 +1,69 @@
+var optionsGeneral = [
+'A Creature Flashed into Play',
+'Altered Card',
+'Storm Count 10+',
+'Hear the Phrase, "In Response"',
+'10 or More +1/+1 Counters',
+'Loss w/ No Permanents in Play',
+'Draw From an Empty Library',
+"Win w/ Opponent's Spell on Stack",
+'Matching Sleeves + Playmat',
+'Win w/ "You Win the Game"',
+'Win at 1 life',
+'3+ Planeswalkers in Play',
+'50+ Damage in 1 Turn',
+'Mulligan to 4 or Less',
+'3+ Board Wipes in 1 Game',
+'3+ Creatures Blocking 1 Attacker'
+];
+
+var optionsJudging = [
+'Marked Cards Discovered',
+'Slow Play Warning',
+'Missed Trigger',
+'Tardiness Penalty',
+'Help Another Judge',
+'Have a Back Up Approved',
+'Address a Question to Avoid OA.',
+'Deck Problem / Deck List Error',
+'No Land Cards by Turn 3+ of the game',
+'Game-Rule Violation',
+'Hidden Card Error',
+'Looking at Extra Cards',
+'Improper Shuffling',
+'Communication Policy Violation',
+'Answer a Question Already Announced'
+];
+
+var optionsStandard =
+[
+'Chapter 3 of a Saga',
+'Hazoret the Fervent Activation',
+'Second "Second Sun"'
+];
+
+var optionsModern = 
+[
+'Karn Activation into Second Karn',
+'4+ Damage Bob Flip',
+'Sad Tarmogoyf (0/1)',
+'KCI Combo',
+'Fetch w/ Fetch in Response'
+];
+
+var optionsLegacy = 
+[
+'Emrakill',
+'Foreign Black Border Dual Land',
+'$1K+ Value Card',
+'Trinisphere in play',
+'Delver Flip "Blind".',
+'2+ Permanents on Turn 0'
+];
+
+var optionsFree = 
+[
+"Drink Water",
+"Push in Chairs",
+"Clean Table Trash"
+];
